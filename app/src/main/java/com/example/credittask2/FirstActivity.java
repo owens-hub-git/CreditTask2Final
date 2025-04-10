c9320d70d89ebec7f8d3507eb26a310fc5141fe3
owens-hub-git <owencostin@bigpond.com>
2025-04-10 23:44:07 +1000

    Initial commit

---
+++ app/src/main/java/com/example/credittask2/FirstActivity.java
@@ -0,0 +1,32 @@
+package com.example.credittask2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_first);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView newTextView = findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("username");
+        newTextView.setText("Welcome " + name + "!");
+    }
+
+}
---
+++ app/src/main/java/com/example/credittask2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.credittask2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usernameEditText;
+    public void jumpClick(View view){
+        Intent intent = new Intent(this, FirstActivity.class);
+        intent.putExtra("username", usernameEditText.getText().toString());
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        usernameEditText = findViewById(R.id.usernameEditText);
+    }
+}
